557d3b95b1b7a758bf902fda649ad292b8503910
peter0617ku <peter0617ku@gmail.com>
2024-05-21 22:48:20 +0800

    update code

--- detect_chessboard.py
+++ detect_chessboard.py
@@ -12,27 +12,27 @@ upper_red = np.array([255, 50, 50])
 # 創建遮罩
 mask = cv2.inRange(img, lower_red, upper_red)
 
-_, binary_image = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)
-
 # 查找輪廓
+_, binary_image = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 print(len(contours))
-position = []
+chess_position = []
+
 # 計算每個輪廓的質心
 for contour in contours:
     M = cv2.moments(contour)
     if M["m00"] != 0:
         cx = int(M["m10"] / M["m00"])
         cy = int(M["m01"] / M["m00"])
-        print(f"Contour center: ({cx}, {cy})")
-        position.append([cx,cy])
+        # print(f"Contour center: ({cx}, {cy})")
+        chess_position.append([cx,cy])
 
 # 先按照 x 的值進行排序
-position.sort(key=lambda pair: pair[0])
+chess_position.sort(key=lambda pair: pair[0])
 
 # 再按照 y 的值進行排序
-position.sort(key=lambda pair: pair[1])
-print(position)
+chess_position.sort(key=lambda pair: pair[1])
+print(chess_position)
 plt.imshow(mask)
 plt.show()
